2f3b654651d73bb089f5667082595f86aec92820
sebastianm1627-eng <sebastian.m1627@gmail.com>
2026-04-07 21:42:15 -0500

    se crea API REST de envios

---
+++ php/api_envios.php
@@ -0,0 +1,15 @@
+<?php
+include("conexion.php");
+
+$sql = "SELECT * FROM envios";
+$resultado = mysqli_query($con, $sql);
+
+$envios = [];
+
+while($fila = mysqli_fetch_assoc($resultado)){
+    $envios[] = $fila;
+}
+
+header('Content-Type: application/json');
+echo json_encode($envios);
+?>
